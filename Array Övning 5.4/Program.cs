a428ca8efbfc76df1e5c7255476755a2ce2a9947
Anton <>
2023-03-29 10:06:50 +0200

    Add project files.

---
+++ Array Övning 5.4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] Årtal = {1990,2000,1500,1990,2024 };
+
+            Console.Write("Skriv in ett årtal : ");
+            int År=Convert.ToInt32(Console.ReadLine());
+            int fanns=0;
+
+            for(int i = 0; i <4 ;i++)
+                if (År ==Årtal[i])
+                {
+                    Console.WriteLine("Ditt årtal finns på index " + i);
+                    fanns++;
+                
+                }
+            if (fanns == 0)
+                Console.WriteLine("Årtalet fanns inte");
+
+        }
+    }
+}
